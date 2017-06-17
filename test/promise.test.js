9224922a8016fb81a912de6331286a05a926a743
Silas Boyd-Wickizer <silas@godaddy.com>
2017-06-17 11:53:06 -0700

    feat(Promise API): Experimental support for a promise-based API
    This preserves the original callback-based "core" implementation and layers the
    promised-based API on top of that core implementation. The goal is start using
    and iterating on the promised-based API even if we expect the underlying
    implementation to evolve (e.g., we could consider replacing the core
    callback-based implementation with a promise one).  See
    https://github.com/godaddy/kubernetes-client/issues/37 for discussion.
    
    Example usage:
    
    ```js
    async function main() {
      try {
        const pods = await core.ns('kube-system').po.get();
        console.log(`Watching: ${ JSON.stringify(pods, null, 2) }`);
        const stream = core.ns('kube-system').po.getStream({ qs: { watch: true }});
        stream.on('data', data => { console.log(data.toString()); });
        stream.on('error', err => { throw err; });
        stream.on('end', () => { console.log('end'); });
      } catch (error) {
        console.log(error);
      }
    }
    main();
    ```

---
+++ test/promise.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const assume = require('assume');
+const nock = require('nock');
+
+const common = require('./common');
+const only = common.only;
+const beforeTestingEach = common.beforeTestingEach;
+
+describe('lib.promise', () => {
+  describe('Core', () => {
+    beforeTestingEach('unit', () => {
+      nock(common.api.url)
+        .get(`/api/v1/namespaces/${ common.currentName }/pods/test-pod`)
+        .reply(200, {
+          kind: 'Pod',
+          metadata: { name: 'test-pod' }
+        });
+    });
+
+    only('unit', '.get returns a Pod via a promise', done => {
+      const pods = common.corePromise.ns.po('test-pod').get();
+      pods.then(object => {
+        assume(object.kind).is.equal('Pod');
+        assume(object.metadata.name).is.equal('test-pod');
+        done();
+      });
+    });
+    only('unit', '.getStream returns the Pod via a stream', done => {
+      const stream = common.corePromise.ns.po('test-pod').getStream();
+      const pieces = [];
+      stream.on('data', data => pieces.push(data.toString()));
+      stream.on('error', err => assume(err).is.falsy());
+      stream.on('end', () => {
+        const object = JSON.parse(pieces.join(''));
+        assume(object.kind).is.equal('Pod');
+        done();
+      });
+    });
+  });
+});
