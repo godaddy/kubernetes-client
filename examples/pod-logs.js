eb0d69877e7c2b02f90d7cf5ba42e75261ce557e
Silas Boyd-Wickizer <silas@godaddy.com>
2018-06-05 21:16:40 -0700

    refactor(examples): fix lints in pod-logs.js example

--- examples/pod-logs.js
+++ examples/pod-logs.js
@@ -5,25 +5,25 @@ const Client = require('kubernetes-client').Client;
 const config = require('kubernetes-client').config;
 
 async function main() {
-    try {
+  try {
 
-        const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
+    const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
 
-        // Pod with single container
-        let logs = await client.api.v1.namespaces("namespace_name").pods('pod_name').log.get();
-        console.log(logs.body);
+    // Pod with single container
+    let logs = await client.api.v1.namespaces('namespace_name').pods('pod_name').log.get();
+    console.log(logs.body);
 
-        // Pod with multiple containers
-        logs = await client.api.v1.namespaces("namespace_name").pods('pod_name').log.get({
-            qs: {
-                container: 'container_name',
-            },
-        });
-        console.log(logs.body);
+    // Pod with multiple containers
+    logs = await client.api.v1.namespaces('namespace_name').pods('pod_name').log.get({
+      qs: {
+        container: 'container_name'
+      }
+    });
+    console.log(logs.body);
 
-    } catch (err) {
-      console.error('Error: ', err);
-    }
+  } catch (err) {
+    console.error('Error: ', err);
   }
+}
 
-  main();
+main();
