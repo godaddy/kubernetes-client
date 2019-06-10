92d97299bfb491278caa7661154c3953020004d3
celiawaggoner <cmwaggoner@gmail.com>
2019-06-10 13:41:30 -0700

    test(patch): add integration test for `.patch` (#492)

---
+++ test-integration/patch.js
@@ -0,0 +1,42 @@
+/* eslint-env mocha */
+'use strict'
+
+const expect = require('chai').expect
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+const env = require('./env')
+
+describe('test-integration/patch', () => {
+  let namespace = null
+  beforeEach(async () => {
+    namespace = await env.setupNamespace()
+  })
+  afterEach(async () => {
+    await env.tearDownNamespace(namespace)
+  })
+
+  it('patches a pod', async () => {
+    let pod
+    const manifest = yaml.safeLoad(fs.readFileSync('./test-integration/busybox-pod.yaml'))
+    const name = manifest.metadata.name
+    const client = await env.getClient()
+    await client.api.v1.namespaces(namespace).pods.post({ body: manifest })
+
+    pod = await client.api.v1.namespaces(namespace).pods(name).get()
+    expect(pod.body.metadata.annotations).to.equal(undefined)
+
+    await client.api.v1.namespaces(namespace).pods(name).patch({
+      body: {
+        metadata: {
+          annotations: {
+            'patch-test0': process.pid.toString()
+          }
+        }
+      }
+    })
+
+    pod = await client.api.v1.namespaces(namespace).pods(name).get()
+    expect(pod.body.metadata.annotations['patch-test0']).equals(process.pid.toString())
+  })
+})
